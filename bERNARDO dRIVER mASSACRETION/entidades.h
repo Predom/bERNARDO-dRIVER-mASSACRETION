3345d6cdfc868b384535d203f4829f6b36b182de
Predom <32460877+Predom@users.noreply.github.com>
2018-11-30 13:16:02 -0200

    Aqueles struct q tu queria 2x

--- bERNARDO dRIVER mASSACRETION/entidades.h
+++ bERNARDO dRIVER mASSACRETION/entidades.h
@@ -32,6 +32,34 @@ struct CONVEXPOLYGON
     int NumVertices;
 };
 
+ struct Player
+{
+
+    struct CONVEXPOLYGON *modelo;    /// endereço do modelo
+
+    struct CONVEXPOLYGON locAtual;   /// para ser usado nas funções de desenho e detecção de colisão
+                                     /// é obtido transformando o modelo pelo ângulo atual
+    float angulo;                    /// em relação ao eixo x
+
+
+};
+
+struct Carro
+{
+    int ID;
+    struct CONVEXPOLYGON *modelo;    /// endereço do modelo
+
+    struct CONVEXPOLYGON locAtual;   /// para ser usado nas funções de desenho e detecção de colisão
+                                     /// é obtido transformando o modelo pelo ângulo atual+ânugo do player e
+                                     /// somando o vetor local.
+
+    struct Vetor2D local;            ///localização em relação ao player
+    float angulo;                    /// em relação ao eixo x
+
+    float dano;
+    float massa;
+
+};
 
 #endif
 
--- bERNARDO dRIVER mASSACRETION/fisica.h
+++ bERNARDO dRIVER mASSACRETION/fisica.h
@@ -11,32 +11,14 @@
  **************************************/
 
 
- struct Player
-{
+///Player func
 
-    struct CONVEXPOLYGON &modelo;    /// endereço do modelo
+void atualizarPlayer(struct Player *P);
 
-    struct CONVEXPOLYGON locAtual;   /// para ser usado nas funções de desenho e detecção de colisão
-                                     /// é obtido transformando o modelo pelo ângulo atual
-    float angulo;                    /// em relação ao eixo x
+///carros func
 
+void atualizaCarro(struct Carro *c);
 
-};
-
-struct Carro
-{
-    int ID;
-    struct CONVEXPOLYGON &modelo;    /// endereço do modelo
-
-    struct CONVEXPOLYGON locAtual;   /// para ser usado nas funções de desenho e detecção de colisão
-                                     /// é obtido transformando o modelo pelo ângulo atual+ânugo do player e
-                                     /// somando o vetor local.
-
-    struct Vetor2D local;            ///localização em relação ao player
-    float angulo;                    /// em relação ao eixo x
-
-    float dano;
-};
 
 
 
