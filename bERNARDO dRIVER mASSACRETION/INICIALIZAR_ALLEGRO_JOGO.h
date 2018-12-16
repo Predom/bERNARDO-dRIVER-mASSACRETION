7f4a3ba8b3adff016f918baa79eb740b6c21f1a2
Théo Pires <theo.v.p.com@gmail.com>
2018-12-16 03:40:43 -0200

    comentando o som

--- bERNARDO dRIVER mASSACRETION/INICIALIZAR_ALLEGRO_JOGO.h
+++ bERNARDO dRIVER mASSACRETION/INICIALIZAR_ALLEGRO_JOGO.h
@@ -15,7 +15,7 @@
 	///AUDIO-------------------------------------------------------------------------------
     vrum = al_load_audio_stream("Sons\\Brum.ogg",255,100);
     al_attach_audio_stream_to_mixer(vrum, al_get_default_mixer());
-    al_set_audio_stream_playmode(vrum, ALLEGRO_PLAYMODE_LOOP);
+    //al_set_audio_stream_playmode(vrum, ALLEGRO_PLAYMODE_LOOP);
 
 	///IMAGENS-----------------------------------------------------------------------------
 	rua_bus = al_load_bitmap("Bitmaps\\Rua_bus.bmp");
